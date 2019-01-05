76a45278c89c9fca5bb93159f31be860fcc921ca
Andreyglass1989 <1989andreyglass@gmail.com>
2019-01-05 23:27:55 +0200

    5.One Hot Array

--- spam-filter/main.py
+++ spam-filter/main.py
@@ -5,8 +5,24 @@ txt = "This is awesome"
 
 txt2 = "This is bad"
 
-bags_of_words = ["This", "is", "awesome", "bad"]
+txt3 = "What a great time"
+
+bags_of_words = ["This", "is", "awesome", "bad", "what", "a", "greate", "time"]
 
 txt_bow = [0, 1, 2]
 
 txt2_bow = [0, 1, 3]
+
+txt3_bow = [4, 5, 6, 7]
+
+#one hot array
+
+#txt_oha = [True, True, True, False, False, False, False, False]
+
+#this based on if  position has value in that string
+
+txt_oha = [1, 1, 1, 0, 0, 0, 0, 0]
+
+txt2_oha = [1, 1, 0, 1, 0, 0, 0, 0]
+
+txt3_oha = [0, 0, 0, 0, 1, 1, 1, 1]
