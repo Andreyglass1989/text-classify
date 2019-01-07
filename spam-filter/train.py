2c880b40deb88e76d0944d7ba1aed396d83251f4
Andreyglass1989 <1989andreyglass@gmail.com>
2019-01-07 16:54:37 +0200

    10.One Hot Array with External Data

--- spam-filter/train.py
+++ spam-filter/train.py
@@ -13,6 +13,19 @@ def file_to_bow(filepath='data/pos.txt'):
 file_to_bow(filepath='data/pos.txt')
 file_to_bow(filepath='data/neg.txt')
 
+def file_to_oha(filepath='data/pos.txt'):
+	my_oha = []
+	with open(filepath, 'r') as f:
+		lines = f.readlines()
+		for line in lines:
+			escaped_line = line.replace('\n', '')
+			oha = to_one_hot(escaped_line)
+			my_oha.append(oha)
+	return my_oha
 
-print(bag)
+
+pos_ohas = file_to_oha(filepath='data/pos.txt')
+neg_ohas = file_to_oha(filepath='data/neg.txt')
+
+print(pos_ohas, neg_ohas)
 
